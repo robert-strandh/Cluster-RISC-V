3d744cad8062183ddc9c59b01a56b906f135cd73
Robert Strandh <robert.strandh@gmail.com>
2025-06-10 13:26:29 +0200

    Define class IMMEDIATE-INSTRUCTION.

--- Code/instructions.lisp
+++ Code/instructions.lisp
@@ -22,3 +22,39 @@
     :initarg :opcode
     :reader opcode)))
 
+(defmethod initialize-instance :after
+    ((instruction register-instruction)
+     &key
+       function-7
+       function-3
+       opcode)
+  (check-type function-7 (unsigned-byte 7))
+  (check-type function-3 (unsigned-byte 3))
+  (check-type opcode (unsigned-byte 7)))
+
+(defclass immediate-instruction (instruction)
+  ((%immediate-value
+    :initarg :immediate-value
+    :reader immediate-value)
+   (%source-register
+    :initarg :source-register
+    :reader source-register)
+   (%function-3
+    :initarg :function-3
+    :reader function-3)
+   (%destination-register
+    :initarg :destination-register
+    :reader destination-register)
+   (%opcode
+    :initarg :opcode
+    :reader opcode)))
+
+(defmethod initialize-instance :after
+    ((instruction immediate-instruction)
+     &key
+       immediate-value
+       function-3
+       opcode)
+  (check-type immediate-value (signed-byte 12))
+  (check-type function-3 (unsigned-byte 3))
+  (check-type opcode (unsigned-byte 7)))

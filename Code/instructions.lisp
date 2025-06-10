7ae596cabf84201d1ff5c925c56d22b74f358762
Robert Strandh <robert.strandh@gmail.com>
2025-06-10 13:38:06 +0200

    Add INTEGER-REGISTER-IMMEDIATE/REGISTER-INSTRUCTION.

--- Code/instructions.lisp
+++ Code/instructions.lisp
@@ -58,3 +58,15 @@
   (check-type immediate-value (signed-byte 12))
   (check-type function-3 (unsigned-byte 3))
   (check-type opcode (unsigned-byte 7)))
+
+(defconstant +opcode-immediate+ #b0010011)
+
+(defclass integer-register-immediate-instruction (immediate-instruction)
+  ()
+  (:default-initargs :opcode +opcode-immediate+))
+
+(defconstant +opcode-register+ #b0110011)
+
+(defclass integer-register-register-instruction (register-instruction)
+  ()
+  (:default-initargs :opcode +opcode-register+))

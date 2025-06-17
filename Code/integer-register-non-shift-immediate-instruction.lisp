0dc7b03fe100297a8e3699b2ad57a3764eebe5cf
Robert Strandh <robert.strandh@gmail.com>
2025-06-17 10:05:48 +0200

    ENCODE method for INTEGER-REGISTER-NON-SHIFT-IMMEDIATE-INSTRUCTION.

--- Code/integer-register-non-shift-immediate-instruction.lisp
+++ Code/integer-register-non-shift-immediate-instruction.lisp
@@ -12,3 +12,10 @@
        immediate-value)
   (check-type immediate-value (signed-byte 12)))
 
+(defmethod encode
+    ((instruction integer-register-non-shift-immediate-instruction))
+  (logior (ash (immediate-value instruction) 20)
+          (ash (register-number (source-register instruction)) 15)
+          (ash (function-3 instruction) 12)
+          (ash (register-number (destination-register instruction)) 7)
+          (opcode instruction)))

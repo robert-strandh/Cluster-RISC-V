f987369a47e696ede928eabedf0bf5351bb10f92
Robert Strandh <robert.strandh@gmail.com>
2025-06-17 10:14:33 +0200

    Add slot FUNCTION-6 to INTEGER-REGISTER-SHIFT-IMMEDIATE-INSTRUCTION.

--- Code/integer-register-shift-immediate-instruction.lisp
+++ Code/integer-register-shift-immediate-instruction.lisp
@@ -2,7 +2,8 @@
 
 (defclass integer-register-shift-immediate-instruction
     (integer-register-immediate-instruction)
-  ((%shift-amount :initarg :shift-amount :reader shift-amount)))
+  ((%shift-amount :initarg :shift-amount :reader shift-amount)
+   (%function-6 :initarg :function-6 :reader function-6)))
 
 (defmethod initialize-instance :after
     ((instruction integer-register-shift-immediate-instruction)

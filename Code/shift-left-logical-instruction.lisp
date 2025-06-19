326296a6df34b8687629efeb56a70c5e55461aec
Robert Strandh <robert.strandh@gmail.com>
2025-06-19 09:46:29 +0200

    Remove obsolete constant definitions.

--- Code/shift-left-logical-instruction.lisp
+++ Code/shift-left-logical-instruction.lisp
@@ -1,9 +1,5 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +shift-left-logical-function-7+ #b0000000)
-
-(defconstant +shift-left-logical-function-3+ #b001)
-
 (defclass shift-left-logical-instruction
     (integer-register-register-shift-left-logical-instruction-mixin
      full-integer-register-register-instruction)

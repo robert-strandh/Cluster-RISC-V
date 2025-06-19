9717a67b4d19b78d58ea162094778befa6b42512
Robert Strandh <robert.strandh@gmail.com>
2025-06-19 09:46:41 +0200

    Make mixin class a superclass and remove obsolete constants.

--- Code/shift-right-logical-instruction.lisp
+++ Code/shift-right-logical-instruction.lisp
@@ -1,12 +1,6 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +shift-right-logical-function-7+ #b0000000)
-
-(defconstant +shift-right-logical-function-3+ #b101)
-
 (defclass shift-right-logical-instruction
-    (full-integer-register-register-instruction)
-  ()
-  (:default-initargs
-   :function-7 +shift-right-logical-function-7+
-   :function-3 +shift-right-logical-function-3+))
+    (integer-register-register-shift-right-logical-instruction-mixin
+     full-integer-register-register-instruction)
+  ())

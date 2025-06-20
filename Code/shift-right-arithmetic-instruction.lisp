a9bd09be3dbdc5535e2e9b66cf74b0f40a2e83b6
Robert Strandh <robert.strandh@gmail.com>
2025-06-20 12:56:55 +0200

    Make mixin class a superclass and remove obsolete constants.

--- Code/shift-right-arithmetic-instruction.lisp
+++ Code/shift-right-arithmetic-instruction.lisp
@@ -1,12 +1,6 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +shift-right-arithmetic-function-7+ #b0100000)
-
-(defconstant +shift-right-arithmetic-function-3+ #b101)
-
 (defclass shift-right-arithmetic-instruction
-    (full-integer-register-register-instruction)
-  ()
-  (:default-initargs
-   :function-7 +shift-right-arithmetic-function-7+
-   :function-3 +shift-right-arithmetic-function-3+))
+    (integer-register-register-shift-right-arithmetic-instruction-mixin
+     full-integer-register-register-instruction)
+  ())

03ecc64d85e21c8cfa56596ec02e432c9c1d0c2c
Robert Strandh <robert.strandh@gmail.com>
2025-07-27 13:02:25 +0200

    Rename instruction to correspond to RISC-V documentation.

--- Code/addi-instruction.lisp
+++ Code/addi-instruction.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +add-immediate-function-3+ #b000)
+(defconstant +addi-function-3+ #b000)
 
-(defclass add-immediate-instruction
+(defclass addi-instruction
     (integer-register-non-shift-immediate-instruction)
   ()
-  (:default-initargs :function-3 +add-immediate-function-3+))
+  (:default-initargs :function-3 +addi-function-3+))

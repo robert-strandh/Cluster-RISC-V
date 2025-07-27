7b29ad58598db9e3600a80f9b1b5caf8d05558de
Robert Strandh <robert.strandh@gmail.com>
2025-07-27 07:41:08 +0200

    Export names of several instruction classes.

--- Code/packages.lisp
+++ Code/packages.lisp
@@ -2,4 +2,27 @@
 
 (defpackage #:cluster-risc-v
   (:use #:common-lisp)
-  (:export))
+  (:export
+   #:add-immediate-instruction
+   #:add-instruction
+   #:add-upper-immediate-to-pc-instruction
+   #:add-word-instruction
+   #:and-immediate-instruction
+   #:and-instruction
+   #:branch-equal-instruction
+   #:branch-greater-or-equal-instruction
+   #:branch-greater-or-equal-unsigned-instruction
+   #:branch-less-than-instruction
+   #:branch-less-than-unsigned-instruction
+   #:branch-not-equal-instruction
+   #:half-integer-register-register-set-less-than-instruction
+   #:half-integer-register-register-set-less-than-unsigned-instruction
+   #:half-integer-register-register-shift-left-logical-instruction
+   #:half-integer-register-register-shift-right-arithmetic-instruction
+   #:half-integer-register-register-shift-right-logical-instruction
+   #:half-integer-register-register-subtract-instruction
+   #:integer-register-register-add-instruction
+   #:integer-register-register-shift-left-logical-instruction
+   #:integer-register-register-shift-right-arithmetic-instruction
+   #:integer-register-register-shifr-right-logical-instruction))
+   

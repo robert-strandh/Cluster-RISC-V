b74cad240e02c5933a3a839f03b511cba486af65
Robert Strandh <robert.strandh@gmail.com>
2025-07-27 13:08:29 +0200

    Rename instruction to correspond to RISC-V documentation.

--- Code/ori-instruction.lisp
+++ Code/ori-instruction.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +or-immediate-function-3+ #b110)
+(defconstant +ori-function-3+ #b110)
 
-(defclass or-immediate-instruction
+(defclass ori-instruction
     (integer-register-non-shift-immediate-instruction)
   ()
-  (:default-initargs :function-3 +or-immediate-function-3+))
+  (:default-initargs :function-3 +ori-function-3+))
 

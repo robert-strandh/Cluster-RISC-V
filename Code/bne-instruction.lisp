e81fb372822d747bcb1752b3e1ac0c7049423ec6
Robert Strandh <robert.strandh@gmail.com>
2025-07-28 11:44:11 +0200

    Rename instructions to correspond to RISC-V documentation.

--- Code/bne-instruction.lisp
+++ Code/bne-instruction.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +branch-not-equal-function-3+ #b001)
+(defconstant +bne-function-3+ #b001)
 
-(defclass branch-not-equal-instruction (branch-instruction)
+(defclass bne-instruction (branch-instruction)
   ())

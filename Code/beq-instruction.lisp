31408108cdaeb94b5976d304e9cb84d4744d145c
Robert Strandh <robert.strandh@gmail.com>
2025-07-28 11:43:31 +0200

    Rename instructions to correspond to RISC-V documentation.

--- Code/beq-instruction.lisp
+++ Code/beq-instruction.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +branch-equal-function-3+ #b000)
+(defconstant +beq-function-3+ #b000)
 
-(defclass branch-equal-instruction (branch-instruction)
+(defclass beq-instruction (branch-instruction)
   ())

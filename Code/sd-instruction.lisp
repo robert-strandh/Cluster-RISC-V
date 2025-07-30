bc916346b48ea95e8e7fb0cdb1ebdd68d4b59d0c
Robert Strandh <robert.strandh@gmail.com>
2025-07-30 12:46:01 +0200

    Make new class STORE-INSTRUCTION a superclass of SD-INSTRUCTION.

--- Code/sd-instruction.lisp
+++ Code/sd-instruction.lisp
@@ -1,25 +1,5 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +opcode-sd+ #b0100011)
-
-(defclass sd-instruction (instruction)
-  ((%immediate-value
-    :initarg :immediate-value
-    :reader immediate-value)
-   (%source-register-1
-    :initarg :source-register-1
-    :reader source-register-1)
-   (%source-register-2
-    :initarg :source-register-2
-    :reader source-register-2)
-   (%function-3
-    :initarg :function-3
-    :reader function-3)))
-
-(defmethod initialize-instance :after
-    ((instruction sd-instruction)
-     &key
-       immediate-value
-       function-3)
-  (check-type immediate-value (signed-byte 12))
-  (check-type function-3 (unsigned-byte 3)))
+(defclass sd-instruction (store-instruction)
+  ()
+  (:default-initarg :width #.(integer-log 8)))

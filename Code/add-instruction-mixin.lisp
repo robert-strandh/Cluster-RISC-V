cbb1b4d9a2263d33295e76e58939adc4d0663c2a
Robert Strandh <robert.strandh@gmail.com>
2025-08-01 16:58:22 +0200

    Rename instruction

--- Code/add-instruction-mixin.lisp
+++ Code/add-instruction-mixin.lisp
@@ -1,11 +1,11 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +integer-register-register-add-function-7+ #b0000000)
+(defconstant +add-function-7+ #b0000000)
 
-(defconstant +integer-register-register-add-function-3+ #b000)
+(defconstant +add-function-3+ #b000)
 
-(defclass integer-register-register-add-instruction-mixin ()
+(defclass add-instruction-mixin ()
   ()
   (:default-initargs
-   :function-7 +integer-register-register-add-function-7+
-   :function-3 +integer-register-register-add-function-3+))
+   :function-7 +add-function-7+
+   :function-3 +add-function-3+))

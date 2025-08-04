b5c49c96956adeac2e596f9ff18edb41298d1015
Robert Strandh <robert.strandh@gmail.com>
2025-08-04 09:13:55 +0200

    Add default initargs and remove obsolete superclass.

--- Code/sltu-instruction.lisp
+++ Code/sltu-instruction.lisp
@@ -1,6 +1,12 @@
 (cl:in-package #:cluster-risc-v)
 
+(defconstant +sltu-function-7+ #b0000000)
+
+(defconstant +sltu-function-3+ #b011)
+
 (defclass sltu-instruction
-    (integer-register-register-set-less-than-unsigned-instruction-mixin
-     full-integer-register-register-instruction)
-  ())
+    (full-integer-register-register-instruction)
+  ()
+  (:default-initargs
+   :function-7 +sltu-function-7+
+   :function-3 +sltu-function-3+))

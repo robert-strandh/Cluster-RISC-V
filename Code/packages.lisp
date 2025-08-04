3ede8f3ccf2cf8f504a7b39b3dbb82419089f774
Robert Strandh <robert.strandh@gmail.com>
2025-08-04 14:55:03 +0200

    Remove obsolete EXPORT entries.

--- Code/packages.lisp
+++ Code/packages.lisp
@@ -14,15 +14,5 @@
    #:branch-greater-or-equal-unsigned-instruction
    #:branch-less-than-instruction
    #:branch-less-than-unsigned-instruction
-   #:branch-not-equal-instruction
-   #:half-integer-register-register-set-less-than-instruction
-   #:half-integer-register-register-set-less-than-unsigned-instruction
-   #:half-integer-register-register-shift-left-logical-instruction
-   #:half-integer-register-register-shift-right-arithmetic-instruction
-   #:half-integer-register-register-shift-right-logical-instruction
-   #:half-integer-register-register-subtract-instruction
-   #:integer-register-register-add-instruction
-   #:integer-register-register-shift-left-logical-instruction
-   #:integer-register-register-shift-right-arithmetic-instruction
-   #:integer-register-register-shifr-right-logical-instruction))
+   #:branch-not-equal-instruction))
    

4430b98dfa958dc61f0555ee88598bcdcb2bf560
Robert Strandh <robert.strandh@gmail.com>
2025-08-05 08:02:49 +0200

    Simplify name of mixin class.

--- Code/integer-register-register-shift-right-arithmetic-instruction-mixin.lisp
+++ Code/integer-register-register-shift-right-arithmetic-instruction-mixin.lisp
@@ -1,13 +1,13 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +integer-register-register-shift-right-arithmetic-function-7+
+(defconstant +sra-function-7+
   #b0100000)
 
-(defconstant +integer-register-register-shift-right-arithmetic-function-3+
+(defconstant +sra-function-3+
   #b101)
 
-(defclass integer-register-register-shift-right-arithmetic-instruction-mixin ()
+(defclass sra-instruction-mixin ()
   ()
   (:default-initargs
-   :function-7 +integer-register-register-shift-right-arithmetic-function-7+
-   :function-3 +integer-register-register-shift-right-arithmetic-function-3+))
+   :function-7 +sra-function-7+
+   :function-3 +sra-function-3+))
--- Code/sra-instruction.lisp
+++ Code/sra-instruction.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:cluster-risc-v)
 
 (defclass sra-instruction
-    (integer-register-register-sra-instruction-mixin
+    (sra-instruction-mixin
      full-integer-register-register-instruction)
   ())

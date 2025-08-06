38461c3b4c2f08231d816dad0fb7ecb4d497b8e6
Robert Strandh <robert.strandh@gmail.com>
2025-08-06 14:30:20 +0200

    Change class name to correspond to RISC-V documentation.

--- Code/integer-register-register-subtract-instruction-mixin.lisp
+++ Code/integer-register-register-subtract-instruction-mixin.lisp
@@ -1,11 +1,11 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +integer-register-register-subtract-function-7+ #b0100000)
+(defconstant +sub-function-7+ #b0100000)
 
-(defconstant +integer-register-register-subtract-function-3+ #b000)
+(defconstant +sub-function-3+ #b000)
 
-(defclass integer-register-register-subtract-instruction-mixin ()
+(defclass sub-instruction-mixin ()
   ()
   (:default-initargs
-   :function-7 +integer-register-register-subtract-function-7+
-   :function-3 +integer-register-register-subtract-function-3+))
+   :function-7 +sub-function-7+
+   :function-3 +sub-function-3+))
--- Code/subw-instruction.lisp
+++ Code/subw-instruction.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:cluster-risc-v)
 
 (defclass subw-instruction
-    (integer-register-register-subtract-instruction-mixin
+    (sub-instruction-mixin
      half-integer-register-register-instruction)
   ())

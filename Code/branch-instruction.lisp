450eca02a9adc45353d015d8822838ee0e0072e3
Robert Strandh <robert.strandh@gmail.com>
2025-08-11 16:20:03 +0200

    Check types of several other initargs.

--- Code/branch-instruction.lisp
+++ Code/branch-instruction.lisp
@@ -21,5 +21,11 @@
 (defmethod initialize-instance :after
     ((instruction branch-instruction)
      &key
-       function-3)
-  (check-type function-3 (unsigned-byte 3)))
+       function-3
+       source-register-1
+       source-register-2
+       target)
+  (check-type function-3 (unsigned-byte 3))
+  (check-type source-register-1 integer-register)
+  (check-type source-register-2 integer-register)
+  (check-type target instruction))

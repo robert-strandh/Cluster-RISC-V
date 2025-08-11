43f92201bb8c6c0ad4817a705b255559340c9a72
Robert Strandh <robert.strandh@gmail.com>
2025-08-11 16:30:03 +0200

    Provide default initargs.

--- Code/beq-instruction.lisp
+++ Code/beq-instruction.lisp
@@ -3,4 +3,5 @@
 (defconstant +beq-function-3+ #b000)
 
 (defclass beq-instruction (branch-instruction)
-  ())
+  ()
+  (:default-initargs :function-3 +beq-function-3+))
--- Code/bne-instruction.lisp
+++ Code/bne-instruction.lisp
@@ -3,4 +3,5 @@
 (defconstant +bne-function-3+ #b001)
 
 (defclass bne-instruction (branch-instruction)
-  ())
+  ()
+  (:default-initargs :function-3 +bne-function-3+))

9422b648c2862dda09195b3cc41bf8d545772825
Robert Strandh <robert.strandh@gmail.com>
2025-08-13 08:11:11 +0200

    New component containing constants for opcodes etc.

---
+++ Code/Common/constants.lisp
@@ -0,0 +1,7 @@
+(cl:in-package #:cluster-risc-v-common)
+
+(defconstant +opcode-load+ #b0000011)
+
+(defconstant +opcode-store+ #b0100011)
+
+(defconstant +opcode-integer-register-register+ #b0110011)

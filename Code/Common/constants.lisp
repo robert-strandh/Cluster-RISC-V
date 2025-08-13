99a88f50e58ccb7674c3c2ecee4482b7a2348eee
Robert Strandh <robert.strandh@gmail.com>
2025-08-13 08:45:38 +0200

    Modify names to correspond to RISC-V terminology.

--- Code/Common/constants.lisp
+++ Code/Common/constants.lisp
@@ -6,6 +6,8 @@
 
 (defconstant +opcode-integer-register-register+ #b0110011)
 
-(defconstant +opcode-immediate-full-word+ #b0010011)
+(defconstant +opcode-immediate-double-word+ #b0010011)
 
-(defconstant +opcode-immediate-half-word+ #b0011011)
+(defconstant +opcode-immediate-word+ #b0011011)
+
+(defconstant +opcode-branch+ #b1100011)

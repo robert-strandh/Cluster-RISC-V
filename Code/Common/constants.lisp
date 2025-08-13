cafea13040906a06f0fc169345541489bacb1840
Robert Strandh <robert.strandh@gmail.com>
2025-08-13 08:14:29 +0200

    Add opcode for immediate instructions.

--- Code/Common/constants.lisp
+++ Code/Common/constants.lisp
@@ -5,3 +5,7 @@
 (defconstant +opcode-store+ #b0100011)
 
 (defconstant +opcode-integer-register-register+ #b0110011)
+
+(defconstant +opcode-immediate-full-word+ #b0010011)
+
+(defconstant +opcode-immediate-half-word+ #b0011011)

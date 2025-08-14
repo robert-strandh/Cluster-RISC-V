1dbcafabcefd25371f855d803fbeb341dfa81abb
Robert Strandh <robert.strandh@gmail.com>
2025-08-14 15:22:37 +0200

    Remove slots FUNCTION-3 and FUNCTION-7.  Define generic functions.

--- Code/register-instruction.lisp
+++ Code/register-instruction.lisp
@@ -1,18 +1,16 @@
 (cl:in-package #:cluster-risc-v)
 
+(defgeneric function-7 (instruction))
+
+(defgeneric function-3 (instruction))
+
 (defclass register-instruction (instruction)
-  ((%function-7
-    :initarg :function-7
-    :reader function-7)
-   (%source-register-1
+  ((%source-register-1
     :initarg :source-register-1
     :reader source-register-1)
    (%source-register-2
     :initarg :source-register-2
     :reader source-register-2)
-   (%function-3
-    :initarg :function-3
-    :reader function-3)
    (%destination-register
     :initarg :destination-register
     :reader destination-register)))

63614518a59f81a2a0c064a0159638f67172dd34
Robert Strandh <robert.strandh@gmail.com>
2025-08-15 11:56:58 +0200

    Rename class to conform to RISC-V terminology.

--- Code/integer-register-register-word-instruction.lisp
+++ Code/integer-register-register-word-instruction.lisp
@@ -1,8 +1,5 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +half-integer-register-register-opcode+ #b0111011)
-
-(defclass half-integer-register-register-instruction
+(defclass integer-register-register-word-instruction
     (integer-register-register-instruction)
-  ()
-  (:default-initargs :opcode +half-integer-register-register-opcode+))
+  ())

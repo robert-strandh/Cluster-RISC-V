6af24b6d3c14cb9c09a2ac25aaf50b367b5379b1
Robert Strandh <robert.strandh@gmail.com>
2025-08-15 11:47:14 +0200

    Add slots and make INSTRUCTION the only superclass.

--- Code/integer-register-register-instruction.lisp
+++ Code/integer-register-register-instruction.lisp
@@ -1,7 +1,15 @@
 (cl:in-package #:cluster-risc-v)
 
-(defclass integer-register-register-instruction (register-instruction)
-  ())
+(defclass integer-register-register-instruction (instruction)
+  ((%source-register-1
+    :initarg :source-register-1
+    :reader source-register-1)
+   (%source-register-2
+    :initarg :source-register-2
+    :reader source-register-2)
+   (%destination-register
+    :initarg :destination-register
+    :reader destination-register)))
 
 (defmethod initialize-instance :after
     ((instruction integer-register-register-instruction)

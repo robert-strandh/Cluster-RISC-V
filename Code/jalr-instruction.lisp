eb8ceff149761301d7767400d5feeda74c9a5b3c
Robert Strandh <robert.strandh@gmail.com>
2025-08-16 14:44:41 +0200

    Remove constant and default initarg.  Use constant from COMMON

--- Code/jalr-instruction.lisp
+++ Code/jalr-instruction.lisp
@@ -1,11 +1,10 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +jalr-opcode+ #b1100111)
-
-(defconstant +jalr-function-3+ #b000)
-
 (defclass jalr-instruction (immediate-instruction)
-  ()
-  (:default-initargs
-   :opcode +jal-opcode+
-   :function-3 +jalr-function-3+))
+  ())
+
+(defmethod opcode ((instruction jalr-instruction))
+  com:+opcode-jalr+)
+
+(defmethod func-3 ((instruction jalr-instruction))
+  com:+func-3-jalr+)

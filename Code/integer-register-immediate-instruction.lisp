3ebae815d101e600a8a5204405cbbb62a6571643
Robert Strandh <robert.strandh@gmail.com>
2025-08-16 14:43:19 +0200

    Remove default initarg and take constant from common system.

--- Code/integer-register-immediate-instruction.lisp
+++ Code/integer-register-immediate-instruction.lisp
@@ -1,7 +1,4 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +opcode-immediate+ #b0010011)
-
 (defclass integer-register-immediate-instruction (immediate-instruction)
-  ()
-  (:default-initargs :opcode +opcode-immediate+))
+  ())

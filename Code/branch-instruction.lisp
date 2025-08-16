cefc9eb30aa5f9ceb4eff552e3e6cbdd9a9879b6
Robert Strandh <robert.strandh@gmail.com>
2025-08-16 14:40:36 +0200

    Remove slot FUNCTION-3.

--- Code/branch-instruction.lisp
+++ Code/branch-instruction.lisp
@@ -13,19 +13,14 @@
     :reader source-register-1)
    (%source-register-2
     :initarg :source-register-2
-    :reader source-register-2)
-   (%function-3
-    :initarg :function-3
-    :reader function-3)))
+    :reader source-register-2)))
 
 (defmethod initialize-instance :after
     ((instruction branch-instruction)
      &key
-       function-3
        source-register-1
        source-register-2
        target)
-  (check-type function-3 (unsigned-byte 3))
   (check-type source-register-1 integer-register)
   (check-type source-register-2 integer-register)
   (check-type target instruction))

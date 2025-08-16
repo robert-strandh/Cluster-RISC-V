abfacce712f2a86cc76a3e3f6aa17e400dfe6b3e
Robert Strandh <robert.strandh@gmail.com>
2025-08-16 14:44:34 +0200

    Rename FUNCTION-3, FUNCTION-7 to FUNC-3 and FUNC-7.

--- Code/integer-register-register-instruction.lisp
+++ Code/integer-register-register-instruction.lisp
@@ -23,9 +23,9 @@
 
 (defmethod encode-instruction
     ((instruction integer-register-register-instruction))
-  (logior (ash (function-7 instruction) 25)
+  (logior (ash (func-7 instruction) 25)
           (ash (register-number (source-register-2 instruction)) 20)
           (ash (register-number (source-register-1 instruction)) 15)
-          (ash (function-3 instruction) 12)
+          (ash (func-3 instruction) 12)
           (ash (register-number (destination-register instruction)) 7)
           (opcode instruction)))

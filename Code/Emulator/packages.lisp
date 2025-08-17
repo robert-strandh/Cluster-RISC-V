08d59c4958eaf87978f61eee55c52e1ee54a3a94
Robert Strandh <robert.strandh@gmail.com>
2025-08-17 15:09:33 +0200

    Shadow WRITE-BYTE.

--- Code/Emulator/packages.lisp
+++ Code/Emulator/packages.lisp
@@ -3,6 +3,6 @@
 (defpackage #:cluster-risc-v-emulator
   (:use #:common-lisp)
   (:local-nicknames (#:com #:cluster-risc-v-common))
-  (:shadow #:read-byte)
+  (:shadow #:read-byte #:write-byte)
   (:export
    #:*client*))

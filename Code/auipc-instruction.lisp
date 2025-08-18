f29b5e2f0ad0912c98da3897c9b3f2912f4e1fbe
Robert Strandh <robert.strandh@gmail.com>
2025-08-18 13:02:42 +0200

    Replace default initarg by method on generic function OPCODE.

--- Code/auipc-instruction.lisp
+++ Code/auipc-instruction.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:cluster-risc-v)
 
-(defconstant +auipc-opcode+ #b0010111)
-
 (defclass auipc-instruction (upper-instruction)
-  ()
-  (:default-initargs :opcode +auipc-opcode+))
+  ())
+
+(defmethod opcode ((instruction auipc-instruction))
+  com:+opcode-auipc+)

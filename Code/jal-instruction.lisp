e074e99e56959c8efa0010c4761f610195b9c92c
Robert Strandh <robert.strandh@gmail.com>
2025-08-19 13:39:34 +0200

    No longer make UPPER-INSTRUCTION a superclass of JAL-INSTRUCTION.

--- Code/jal-instruction.lisp
+++ Code/jal-instruction.lisp
@@ -1,7 +1,12 @@
 (cl:in-package #:cluster-risc-v)
 
 (defclass jal-instruction (upper-instruction)
-  ())
+  ((%offset
+    :initarg :offset
+    :reader offset)
+   (%destination-register
+    :initarg :destination-register
+    :reader destination-register)))
 
 (defmethod opcode ((instruction jal-instruction))
   com:+opcode-jal+)

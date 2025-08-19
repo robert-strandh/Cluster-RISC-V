34e30862a6cf10e5523424e4491a65224f9a36ec
Robert Strandh <robert.strandh@gmail.com>
2025-08-19 13:38:06 +0200

    No longer make UPPER-INSTRUCTION a superclass of LUI-INSTRUCTION.

--- Code/lui-instruction.lisp
+++ Code/lui-instruction.lisp
@@ -1,7 +1,12 @@
 (cl:in-package #:cluster-risc-v)
 
-(defclass lui-instruction (upper-instruction)
-  ())
+(defclass lui-instruction (instruction)
+  ((%immediate-value
+    :initarg :immediate-value
+    :reader immediate-value)
+   (%destination-register
+    :initarg :destination-register
+    :reader destination-register)))
 
 (defmethod opcode ((instruction lui-instruction))
   com:+opcode-lui+)

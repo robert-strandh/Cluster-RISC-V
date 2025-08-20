039b80a01cfc48eba0ece5d60503e5c393d977ca
Robert Strandh <robert.strandh@gmail.com>
2025-08-20 15:52:33 +0200

    Define method on ENCODE-INSTRUCTION.

--- Code/upper-instruction.lisp
+++ Code/upper-instruction.lisp
@@ -15,4 +15,8 @@
        destination-register)
   (check-type destination-register integer-register)
   (check-type immediate-value (signed-byte 32))
-  (assert (zerop (logand immediate-value #.(1- (expt 2 12)))))
+  (assert (zerop (logand immediate-value #.(1- (expt 2 12))))))
+
+(defmethod encode-instruction ((instruction upper-instruction))
+  (logior (ash (immediate-value instruction) 12)
+          (ash (register-number (destination-register inst

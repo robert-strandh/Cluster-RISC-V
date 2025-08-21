82d34b2a74695d1cae266711b7f478b18223c6c4
Robert Strandh <robert.strandh@gmail.com>
2025-08-21 15:49:28 +0200

    Finsish method on ENCODE-INSTRUCTION and improve code layout.

--- Code/upper-instruction.lisp
+++ Code/upper-instruction.lisp
@@ -10,13 +10,12 @@
 
 (defmethod initialize-instance :after
     ((instruction upper-instruction)
-     &key
-       immediate-value
-       destination-register)
+     &key immediate-value destination-register)
   (check-type destination-register integer-register)
   (check-type immediate-value (signed-byte 32))
   (assert (zerop (logand immediate-value #.(1- (expt 2 12))))))
 
 (defmethod encode-instruction ((instruction upper-instruction))
-  (logior (ash (immediate-value instruction) 12)
-          (ash (register-number (destination-register inst
+  (logior (immediate-value instruction)
+          (ash (register-number (destination-register instruction)) 7)
+          (opcode instruction)))

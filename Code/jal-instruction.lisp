f3c99301d5f51a2bddb003127b4bdea61d9f35b2
Robert Strandh <robert.strandh@gmail.com>
2025-08-21 15:50:04 +0200

    Define :AFTER method on INITIALIZE-INSTANCE for JAL-INSTRUCTION.

--- Code/jal-instruction.lisp
+++ Code/jal-instruction.lisp
@@ -8,5 +8,11 @@
     :initarg :destination-register
     :reader destination-register)))
 
+(defmethod initialize-instance :after
+    ((instruction jal-instruction) &key offset destination-register)
+  (check-type destination-register integer-register)
+  (check-type offset (signed-byte 21))
+  (assert (evenp offset)))
+
 (defmethod opcode ((instruction jal-instruction))
   com:+opcode-jal+)

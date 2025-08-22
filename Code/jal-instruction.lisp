97211a54824ed32145b4bdee89ee42b8c73bb82f
Robert Strandh <robert.strandh@gmail.com>
2025-08-22 15:28:18 +0200

    Define method on ENCODE-INSTRUCTION for JAL-INSTRUCTION.

--- Code/jal-instruction.lisp
+++ Code/jal-instruction.lisp
@@ -16,3 +16,12 @@
 
 (defmethod opcode ((instruction jal-instruction))
   com:+opcode-jal+)
+
+(defmethod encode-instruction ((instruction jal-instruction))
+  (let ((offset (offset instruction)))
+    (logior (ash (ldb (byte 1 20) offset) 31)
+            (ash (ldb (byte 10 10) offset) 21)
+            (ash (ldb (byte 1 11) offset) 20)
+            (ash (ldb (byte 8 19) offset) 12)
+            (ash (register-number (destination-register instruction)) 7)
+            (opcode instruction))))

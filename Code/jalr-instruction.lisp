c7f8d2d1e93b37eb10f66de19fed67e400b8a11e
Robert Strandh <robert.strandh@gmail.com>
2025-08-23 14:40:58 +0200

    Define :AFTER method on INITIALIZE-INSTANCE checking OFFSET.

--- Code/jalr-instruction.lisp
+++ Code/jalr-instruction.lisp
@@ -3,6 +3,10 @@
 (defclass jalr-instruction (immediate-instruction)
   ((%offset :initarg :offset :reader offset)))
 
+(defmethod initialize-instance :after
+    ((instruction jalr-instruction) &key offset)
+  (check-type offset (signed-byte 12)))
+
 (defmethod opcode ((instruction jalr-instruction))
   com:+opcode-jalr+)
 

fc9f3f1d77dfc928b1bf4522ffbd43f011306b4e
Robert Strandh <robert.strandh@gmail.com>
2025-08-24 15:21:17 +0200

    Make SHIFT-IMMEDIATE-DOUBLE-INSTRUCTION a superclass.

--- Code/srai-instruction.lisp
+++ Code/srai-instruction.lisp
@@ -1,7 +1,10 @@
 (cl:in-package #:cluster-risc-v)
 
-(defclass srai-instruction (integer-register-immediate-double-instruction)
+(defclass srai-instruction (shift-immediate-double-instruction)
   ())
 
 (defmethod func-3 ((instruction srai-instruction))
   com:+func-3-srai+)
+
+(defmethod func-7 ((instruction srai-instruction))
+  com:+func-7-srai+)
--- Code/srli-instruction.lisp
+++ Code/srli-instruction.lisp
@@ -1,7 +1,10 @@
 (cl:in-package #:cluster-risc-v)
 
-(defclass srli-instruction (integer-register-immediate-double-instruction)
+(defclass srli-instruction (shift-immediate-double-instruction)
   ())
 
 (defmethod func-3 ((instruction srli-instruction))
   com:+func-3-srli+)
+
+(defmethod func-7 ((instruction srli-instruction))
+  com:+func-7-srli+)

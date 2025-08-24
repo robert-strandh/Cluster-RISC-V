c5ce8200df1ba64a31e35b7310b02e8309874281
Robert Strandh <robert.strandh@gmail.com>
2025-08-24 14:41:19 +0200

    Define methods on INITIALIZE-INSTANCE and ENCODE-INSTRUCTION.

--- Code/slli-instruction.lisp
+++ Code/slli-instruction.lisp
@@ -1,7 +1,18 @@
 (cl:in-package #:cluster-risc-v)
 
 (defclass slli-instruction (integer-register-immediate-double-instruction)
-  ())
+  ((%shift-amount :initarg :shift-amount :reader shift-amount)))
+
+(defmethod initialize-instance :after
+    ((instruction slli-instruction) &key shift-amount)
+  (check-type shift-amount (unsigned-byte 6)))
 
 (defmethod func-3 ((instruction slli-instruction))
   com:+func-3-slli+)
+
+(defmethod encode-instruction ((instruction slli-instruction))
+  (logior (ash (shift-amount instruction) 20)
+          (ash (register-number (source-register instruction)) 15)
+          (ash (func-3 instruction) 12)
+          (ash (register-number (destination-register instruction)) 7)
+          (opcode instruction)))

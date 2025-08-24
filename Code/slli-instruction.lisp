88957d0a19793b4bd4a267869da7b2a86843acce
Robert Strandh <robert.strandh@gmail.com>
2025-08-24 15:19:02 +0200

    Make SHIFT-IMMEDIATE-DOUBLE-INSTRUCTION a superclass.

--- Code/slli-instruction.lisp
+++ Code/slli-instruction.lisp
@@ -1,18 +1,10 @@
 (cl:in-package #:cluster-risc-v)
 
-(defclass slli-instruction (integer-register-immediate-double-instruction)
-  ((%shift-amount :initarg :shift-amount :reader shift-amount)))
-
-(defmethod initialize-instance :after
-    ((instruction slli-instruction) &key shift-amount)
-  (check-type shift-amount (unsigned-byte 6)))
+(defclass slli-instruction (shift-immediate-double-instruction)
+  ())
 
 (defmethod func-3 ((instruction slli-instruction))
   com:+func-3-slli+)
 
-(defmethod encode-instruction ((instruction slli-instruction))
-  (logior (ash (shift-amount instruction) 20)
-          (ash (register-number (source-register instruction)) 15)
-          (ash (func-3 instruction) 12)
-          (ash (register-number (destination-register instruction)) 7)
-          (opcode instruction)))
+(defmethod func-7 ((instruction slli-instruction))
+  com:+func-7-slli+)

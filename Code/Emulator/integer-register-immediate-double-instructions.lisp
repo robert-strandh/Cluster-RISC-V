3a8a60f9697ee37ecf5a68b1683346ed23c2be06
Robert Strandh <robert.strandh@gmail.com>
2025-08-27 17:40:19 +0200

    Define EXECUTE-SLLI-INSTRUCTION.

--- Code/Emulator/integer-register-immediate-double-instructions.lisp
+++ Code/Emulator/integer-register-immediate-double-instructions.lisp
@@ -8,6 +8,13 @@
                 (sign-extend
                  (integer-register-contents source-register))))))
 
+(defun execute-slli-instruction
+    (shift-amount source-register destination-register)
+  (setf (integer-register-contents destination-register)
+        (ldb (byte 64 0)
+             (ash (integer-register-contents source-register)
+                  shift-amount))))
+
 (defun execute-integer-register-immediate-instruction
     (raw-immediate-value func-3 source-register destination-register)
   (ecase func-3
@@ -15,7 +22,10 @@
      (execute-addi-instruction
       (sign-extend-12 raw-immediate-value)
       source-register destination-register))
-    (#b001)
+    (#b001
+     (execute-slli-instruction
+      (ldb (byte 6 0) raw-immediate-value)
+      source-register destination-register))
     (#b010)
     (#b011)
     (#b100)

d9861e6f66cad408be7b405e991cb9e161511ade
Robert Strandh <robert.strandh@gmail.com>
2025-08-27 17:35:10 +0200

    Define EXECUTE-ADDI-INSTRUCTION.

--- Code/Emulator/integer-register-immediate-double-instructions.lisp
+++ Code/Emulator/integer-register-immediate-double-instructions.lisp
@@ -1,5 +1,24 @@
 (cl:in-package #:cluster-risc-v-emulator)
 
+(defun execute-addi-instruction
+    (immediate-value source-register destination-register)
+  (setf (integer-register-contents destination-register)
+        (ldb (byte 64 0)
+             (+ immediate-value
+                (sign-extend
+                 (integer-register-contents source-register))))))
+
 (defun execute-integer-register-immediate-instruction
-    ()
-  nil)
+    (raw-immediate-value func-3 source-register destination-register)
+  (ecase func-3
+    (#b000
+     (execute-addi-instruction
+      (sign-extend-12 raw-immediate-value)
+      source-register destination-register))
+    (#b001)
+    (#b010)
+    (#b011)
+    (#b100)
+    (#b101)
+    (#b110)
+    (#b111)))

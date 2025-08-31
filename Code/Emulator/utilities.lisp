54ecb3b531b49e5aa0f28b4abfd5ca8a73d6fcb9
Robert Strandh <robert.strandh@gmail.com>
2025-08-31 14:52:54 +0200

    Define several functions for sign extension.

--- Code/Emulator/utilities.lisp
+++ Code/Emulator/utilities.lisp
@@ -1,15 +1,21 @@
 (cl:in-package #:cluster-risc-v-emulator)
 
-;;; Take a 64-bit unsigned integer and consider the topmost bit as a
-;;; sign bit.  According to the value of the sign bit return a
-;;; positive or a negative number that is represented by the unsigned
-;;; integer.
-(defun sign-extend (unsigned-byte)
-  (if (>= unsigned-byte #.(expt 2 63))
-      (- unsigned-byte #.(expt 2 64))
+(defun sign-extend (size unsigned-byte)
+  (if (>= unsigned-byte (expt 2 (1- size)))
+      (- unsigned-byte (expt 2 size))
       unsigned-byte))
 
-(defun sign-extend-12 (raw-immediate-value)
-  (if (>= raw-immediate-value #.(expt 2 11))
-      (- raw-immediate-value #.(expt 2 12))
-      raw-immediate-value))
+(defun sign-extend-64 (unsigned-byte)
+  (sign-extend 64 unsigned-byte))
+
+(defun sign-extend-12 (unsigned-byte)
+  (sign-extend 12 unsigned-byte))
+
+(defun sign-extend-8 (unsigned-byte)
+  (sign-extend 8 unsigned-byte))
+
+(defun sign-extend-16 (unsigned-byte)
+  (sign-extend 16 unsigned-byte))
+
+(defun sign-extend-32 (unsigned-byte)
+  (sign-extend 32 unsigned-byte))

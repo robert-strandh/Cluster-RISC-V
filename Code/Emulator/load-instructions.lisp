89aa75d70d0fe551c6a48a76af8a76185b046ed7
Robert Strandh <robert.strandh@gmail.com>
2025-08-31 14:55:04 +0200

    Define EXECUTE-LH-INSTRUCTION.

--- Code/Emulator/load-instructions.lisp
+++ Code/Emulator/load-instructions.lisp
@@ -6,6 +6,13 @@
          (value (read-byte *client* *default-memory* address)))
     (setf (integer-register-contents destination-register)
           (sign-extend-8 value))))
+
+(defun execute-lh-instruction (offset source-register destination-register)
+  (let* ((address (+ (integer-register-contents source-register)
+                     offset))
+         (value (read-half *client* *default-memory* address)))
+    (setf (integer-register-contents destination-register)
+          (sign-extend-16 value))))
                  
 
 (defun execute-load-instruction
@@ -13,7 +20,8 @@
   (ecase width
     (#b000
      (execute-lb-instruction offset source-register destination-register))
-    (#b001)
+    (#b001
+     (execute-lh-instruction offset source-register destination-register))
     (#b010)
     (#b011)
     (#b100)

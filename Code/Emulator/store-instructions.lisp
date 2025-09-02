59b640cd0bcb7fcc402de42d3136685fafe4aa68
Robert Strandh <robert.strandh@gmail.com>
2025-09-02 13:47:12 +0200

    Define functions for storing byte, half, word, and double.

--- Code/Emulator/store-instructions.lisp
+++ Code/Emulator/store-instructions.lisp
@@ -1,9 +1,33 @@
 (cl:in-package #:cluster-risc-v-emulator)
 
+(defun execute-sb-instruction (address source-register)
+  (let* ((contents (integer-register-contents source-register))
+         (value (ldb (byte 8 0) contents)))
+    (write-byte *client* *default-memory* address value)))
+
+(defun execute-sh-instruction (address source-register)
+  (let* ((contents (integer-register-contents source-register))
+         (value (ldb (byte 16 0) contents)))
+    (write-half *client* *default-memory* address value)))
+
+(defun execute-sw-instruction (address source-register)
+  (let* ((contents (integer-register-contents source-register))
+         (value (ldb (byte 32 0) contents)))
+    (write-word *client* *default-memory* address value)))
+
+(defun execute-sd-instruction (address source-register)
+  (let ((contents (integer-register-contents source-register)))
+    (write-double *client* *default-memory* address contents)))
+
 (defun execute-store-instruction
-    (widh offset base-register source-register)
-  (declare (ignore widh source-register))
+    (width offset base-register source-register)
   (let ((address (+ (integer-register-contents base-register) offset)))
-    (declare (ignore address))))
-
-
+    (ecase width
+      (#b000
+       (execute-sb-instruction address source-register))
+      (#b001
+       (execute-sh-instruction address source-register))
+      (#b010
+       (execute-sw-instruction address source-register))
+      (#b011
+       (execute-sd-instruction address source-register)))))

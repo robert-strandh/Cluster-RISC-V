4b0f271d564010478a7c5b1ac74258028894fd57
Robert Strandh <robert.strandh@gmail.com>
2025-09-03 15:27:29 +0200

    Call EXECUTE-STORE-INSTRUCTION and rename registers.

--- Code/Emulator/execute-instruction.lisp
+++ Code/Emulator/execute-instruction.lisp
@@ -5,11 +5,20 @@
     (#b0000011
      (let ((offset (ldb (byte 12 20) instruction))
            (width (ldb (byte 3 12) instruction))
-           (source-register (ldb (byte 5 16) instruction))
+           (base-register (ldb (byte 5 16) instruction))
            (destination-register (ldb (byte 5 7) instruction)))
        (execute-load-instruction
         width offset
-        source-register destination-register)))
+        base-register destination-register)))
+    (#b0100011
+     (let ((offset (logior (ldb (byte 7 25) instruction)
+                           (ldb (byte 5 7) instruction)))
+           (width (ldb (byte 3 12) instruction))
+           (source-register (ldb (byte 5 20) instruction))
+           (base-register (ldb (byte 5 16) instruction)))
+       (execute-store-instruction
+        width offset
+        base-register source-register)))
     (#b0110011
      (let ((func-7 (ldb (byte 7 25) instruction))
            (func-3 (ldb (byte 3 12) instruction))

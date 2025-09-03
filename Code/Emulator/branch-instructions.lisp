f291cf13925bdc003438d0719510b8bb500900db
Robert Strandh <robert.strandh@gmail.com>
2025-09-03 15:47:10 +0200

    Define EXECUTE-BRANCH-INSTRUCTION.

--- Code/Emulator/branch-instructions.lisp
+++ Code/Emulator/branch-instructions.lisp
@@ -39,3 +39,19 @@
         (value2 (integer-register-contents source-register-2)))
     (when (>= value1 value2)
       (incf *pc* offset))))
+
+(defun execute-branch-instruction
+    (offset func-3 source-register-1 source-register-2)
+  (ecase func-3
+    (#b000
+     (execute-beq-instruction offset source-register-1 source-register-2))
+    (#b001
+     (execute-bne-instruction offset source-register-1 source-register-2))
+    (#b100
+     (execute-blt-instruction offset source-register-1 source-register-2))
+    (#b101
+     (execute-bge-instruction offset source-register-1 source-register-2))
+    (#b110
+     (execute-bltu-instruction offset source-register-1 source-register-2))
+    (#b111
+     (execute-bgeu-instruction offset source-register-1 source-register-2))))

33b638ab9876b4d14b374a2711a13b8ee46ac17a
Robert Strandh <robert.strandh@gmail.com>
2025-09-05 17:02:24 +0200

    Define EXECUTE-JALR-INSTRUCTION.

--- Code/Emulator/jump-instructions.lisp
+++ Code/Emulator/jump-instructions.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:cluster-risc-v-emulator)
+
+(defun execute-jalr-instruction (offset base-register destination-register)
+  (let* ((base (integer-register-contents base-register))
+         (new-pc (+ offset base *pc*)))
+    (setf (integer-register-contents destination-register)
+          (+ *pc* 4))
+    (setf *pc* new-pc)))

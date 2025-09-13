160cf623c763aa0f80c220a21b27463d7c2c9d4a
Robert Strandh <robert.strandh@gmail.com>
2025-09-13 13:21:23 +0200

    Include methods on FUNC-3 and FUNC-7, for related instructions.

--- Code/Assembler/integer-register-register-word-instructions.lisp
+++ Code/Assembler/integer-register-register-word-instructions.lisp
@@ -1,9 +1,35 @@
 (cl:in-package #:cluster-risc-v)
 
-(defclass integer-register-register-word-instruction
-    (integer-register-register-instruction)
-  ())
-
 (defmethod opcode
-    ((instruction integer-register-register-word-instruction))
+    ((instruction ins:integer-register-register-word-instruction))
   com:+opcode-integer-register-register-word+)
+
+(defmethod func-7 ((instruction ins:addw-instruction))
+  com:+func-7-addw+)
+
+(defmethod func-3 ((instruction ins:addw-instruction))
+  com:+func-3-addw+)
+
+(defmethod func-7 ((instruction ins:subw-instruction))
+  com:+func-7-subw+)
+
+(defmethod func-3 ((instruction ins:subw-instruction))
+  com:+func-3-subw+)
+
+(defmethod func-7 ((instruction ins:sllw-instruction))
+  com:+func-7-sllw+)
+
+(defmethod func-3 ((instruction ins:sllw-instruction))
+  com:+func-3-sllw+)
+
+(defmethod func-7 ((instruction ins:srlw-instruction))
+  com:+func-7-srlw+)
+
+(defmethod func-3 ((instruction ins:srlw-instruction))
+  com:+func-3-srlw+)
+
+(defmethod func-7 ((instruction ins:sraw-instruction))
+  com:+func-7-sraw+)
+
+(defmethod func-3 ((instruction ins:sraw-instruction))
+  com:+func-3-sraw+)

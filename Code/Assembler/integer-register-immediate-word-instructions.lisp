c839db68375a7514857eb6136853fd5559a07b3c
Robert Strandh <robert.strandh@gmail.com>
2025-09-13 13:37:29 +0200

    Include methods on FUNC-3 and FUNC-7, for related instructions.

--- Code/Assembler/integer-register-immediate-word-instructions.lisp
+++ Code/Assembler/integer-register-immediate-word-instructions.lisp
@@ -1,9 +1,35 @@
 (cl:in-package #:cluster-risc-v)
 
-(defclass integer-register-immediate-word-instruction
-    (integer-register-immediate-instruction)
-  ())
-
 (defmethod opcode
-    ((instruction integer-register-immediate-word-instruction))
+    ((instruction ins:integer-register-immediate-word-instruction))
   com:+opcode-immediate-word+)
+
+(defmethod func-3 ((instruction ins:addiw-instruction))
+  com:+func-3-addiw+)
+
+(defmethod encode-instruction
+    ((instruction ins:shift-immediate-word-instruction))
+  (logior (ash (func-7 instruction) 26)
+          (ash (ins:shift-amount instruction) 20)
+          (ash (ins:register-number (ins:source-register instruction)) 15)
+          (ash (func-3 instruction) 12)
+          (ash (ins:register-number (ins:destination-register instruction)) 7)
+          (opcode instruction)))
+
+(defmethod func-3 ((instruction ins:slliw-instruction))
+  com:+func-3-slliw+)
+
+(defmethod func-7 ((instruction ins:slliw-instruction))
+  com:+func-7-slliw+)
+
+(defmethod func-3 ((instruction ins:srliw-instruction))
+  com:+func-3-srliw+)
+
+(defmethod func-7 ((instruction ins:srliw-instruction))
+  com:+func-7-srliw+)
+
+(defmethod func-3 ((instruction ins:sraiw-instruction))
+  com:+func-3-sraiw+)
+
+(defmethod func-7 ((instruction ins:sraiw-instruction))
+  com:+func-7-sraiw+)

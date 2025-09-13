7832b5f6fd1352d5507fa330d76a84c59193e443
Robert Strandh <robert.strandh@gmail.com>
2025-09-13 13:16:15 +0200

    Include methods on FUNC-3 and FUNC-7, for related instructions.

--- Code/Assembler/integer-register-register-double-instructions.lisp
+++ Code/Assembler/integer-register-register-double-instructions.lisp
@@ -1,9 +1,65 @@
 (cl:in-package #:cluster-risc-v)
 
-(defclass integer-register-register-double-instruction
-    (integer-register-register-instruction)
-  ())
-
 (defmethod opcode
-    ((instruction integer-register-register-double-instruction))
+    ((instruction ins:integer-register-register-double-instruction))
   com:+opcode-integer-register-register-double+)
+
+(defmethod func-7 ((instruction ins:add-instruction))
+  com:+func-7-add+)
+
+(defmethod func-3 ((instruction ins:add-instruction))
+  com:+func-3-add+)
+
+(defmethod func-7 ((instruction ins:sub-instruction))
+  com:+func-7-sub+)
+
+(defmethod func-3 ((instruction ins:sub-instruction))
+  com:+func-3-sub+)
+
+(defmethod func-7 ((instruction ins:sll-instruction))
+  com:+func-7-sll+)
+
+(defmethod func-3 ((instruction ins:sll-instruction))
+  com:+func-3-sll+)
+
+(defmethod func-7 ((instruction ins:srl-instruction))
+  com:+func-7-srl+)
+
+(defmethod func-3 ((instruction ins:srl-instruction))
+  com:+func-3-srl+)
+
+(defmethod func-7 ((instruction ins:sra-instruction))
+  com:+func-7-sra+)
+
+(defmethod func-3 ((instruction ins:sra-instruction))
+  com:+func-3-sra+)
+
+(defmethod func-7 ((instruction ins:slt-instruction))
+  com:+func-7-slt+)
+
+(defmethod func-3 ((instruction ins:slt-instruction))
+  com:+func-3-slt+)
+
+(defmethod func-7 ((instruction ins:sltu-instruction))
+  com:+func-7-sltu+)
+
+(defmethod func-3 ((instruction ins:sltu-instruction))
+  com:+func-3-sltu+)
+
+(defmethod func-7 ((instruction ins:and-instruction))
+  com:+func-7-and+)
+
+(defmethod func-3 ((instruction ins:and-instruction))
+  com:+func-3-and+)
+
+(defmethod func-7 ((instruction ins:or-instruction))
+  com:+func-7-or+)
+
+(defmethod func-3 ((instruction ins:or-instruction))
+  com:+func-3-or+)
+
+(defmethod func-7 ((instruction ins:xor-instruction))
+  com:+func-7-xor+)
+
+(defmethod func-3 ((instruction ins:xor-instruction))
+  com:+func-3-xor+)

c78b52773cd9589dd801bd196a8fa8c0c4b3f1cf
Robert Strandh <robert.strandh@gmail.com>
2025-09-13 13:05:05 +0200

    No longer export names of instruction classes.

--- Code/Assembler/packages.lisp
+++ Code/Assembler/packages.lisp
@@ -2,17 +2,6 @@
 
 (defpackage #:cluster-risc-v
   (:use #:common-lisp)
-  (:local-nicknames (#:com #:cluster-risc-v-common))
-  (:export
-   #:addi-instruction
-   #:add-instruction
-   #:add-upper-immediate-to-pc-instruction
-   #:add-word-instruction
-   #:andi-instruction
-   #:and-instruction
-   #:branch-equal-instruction
-   #:branch-greater-or-equal-instruction
-   #:branch-greater-or-equal-unsigned-instruction
-   #:branch-less-than-instruction
-   #:branch-less-than-unsigned-instruction
-   #:branch-not-equal-instruction))
+  (:local-nicknames (#:com #:cluster-risc-v-common)
+                    (#:ins #:cluster-risc-v-instructions))
+  (:export))

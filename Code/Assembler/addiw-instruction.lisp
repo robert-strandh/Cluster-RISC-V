a830ddb4cc6c4f696349184b2cb3a6aee23a2190
Robert Strandh <robert.strandh@gmail.com>
2025-09-13 13:37:21 +0200

    Remove obsolete components.

--- Code/Assembler/addiw-instruction.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:cluster-risc-v)
-
-(defclass addiw-instruction (integer-register-immediate-word-instruction)
-  ())
-
-(defmethod func-3 ((instruction addiw-instruction))
-  com:+func-3-addiw+)

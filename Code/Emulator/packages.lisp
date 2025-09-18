8e90c21dd5d3b182414c7fd96cfe58e56a33b0fb
Robert Strandh <robert.strandh@gmail.com>
2025-09-18 14:15:37 +0200

    Define package-local nickname for CLUSTER-RISC-V-INSTRUCTIONS.

--- Code/Emulator/packages.lisp
+++ Code/Emulator/packages.lisp
@@ -2,7 +2,8 @@
 
 (defpackage #:cluster-risc-v-emulator
   (:use #:common-lisp)
-  (:local-nicknames (#:com #:cluster-risc-v-common))
+  (:local-nicknames (#:com #:cluster-risc-v-common)
+                    (#:ins #:cluster-risc-v-instructions)
   (:shadow #:read-byte #:write-byte)
   (:export
    #:*client*))

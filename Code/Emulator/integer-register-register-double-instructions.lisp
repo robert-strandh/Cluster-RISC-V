43edd990a426b35338c53dacf773371f10d39106
Robert Strandh <robert.strandh@gmail.com>
2025-09-18 14:29:50 +0200

    Define method on execute-instruction for SUB-INSTRUCTION.

--- Code/Emulator/integer-register-register-double-instructions.lisp
+++ Code/Emulator/integer-register-register-double-instructions.lisp
@@ -5,3 +5,21 @@
          (source-register-2 (ins:source-register-2 ,instruction-name))
          (destination-register (ins:destination-register ,instruction-name)))
      ,@body))
+
+(defmethod execute-instruction ((instruction ins:add-instruction))
+  (with-three-registers instruction
+    (setf (integer-register-contents destination-register)
+          (ldb (byte 64 0)
+               (+ (sign-extend-64
+                   (integer-register-contents source-register-1))
+                  (sign-extend-64
+                   (integer-register-contents source-register-2)))))))
+
+(defmethod execute-instruction ((instruction ins:sub-instruction))
+  (with-three-registers instruction
+    (setf (integer-register-contents destination-register)
+          (ldb (byte 64 0)
+               (- (sign-extend-64
+                   (integer-register-contents source-register-1))
+                  (sign-extend-64
+                   (integer-register-contents source-register-2)))))))

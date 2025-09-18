0409898836b5de13b5f0760fdab72f8fbf1489f2
Robert Strandh <robert.strandh@gmail.com>
2025-09-18 14:28:06 +0200

    Supply missing closing parenthesis.

--- Code/Emulator/packages.lisp
+++ Code/Emulator/packages.lisp
@@ -3,7 +3,7 @@
 (defpackage #:cluster-risc-v-emulator
   (:use #:common-lisp)
   (:local-nicknames (#:com #:cluster-risc-v-common)
-                    (#:ins #:cluster-risc-v-instructions)
+                    (#:ins #:cluster-risc-v-instructions))
   (:shadow #:read-byte #:write-byte)
   (:export
    #:*client*))

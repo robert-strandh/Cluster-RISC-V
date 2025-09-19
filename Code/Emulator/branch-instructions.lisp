3ddfb86cd9889172e5524ad2e43776ae862b6608
Robert Strandh <robert.strandh@gmail.com>
2025-09-19 18:05:50 +0200

    Define macro WITH-TARGET-AND-SOURCE-REGISTER-VALUES.

--- Code/Emulator/branch-instructions.lisp
+++ Code/Emulator/branch-instructions.lisp
@@ -1,57 +1,9 @@
 (cl:in-package #:cluster-risc-v-emulator)
 
-(defun execute-beq-instruction (offset source-register-1 source-register-2)
-  (let ((value1 (integer-register-contents source-register-1))
-        (value2 (integer-register-contents source-register-2)))
-    (when (= value1 value2)
-      (incf *pc* offset))))
-
-(defun execute-bne-instruction (offset source-register-1 source-register-2)
-  (let ((value1 (integer-register-contents source-register-1))
-        (value2 (integer-register-contents source-register-2)))
-    (unless (= value1 value2)
-      (incf *pc* offset))))
-
-(defun execute-blt-instruction (offset source-register-1 source-register-2)
-  (let ((value1
-          (sign-extend-64 (integer-register-contents source-register-1)))
-        (value2
-          (sign-extend-64 (integer-register-contents source-register-2))))
-    (when (< value1 value2)
-      (incf *pc* offset))))
-
-(defun execute-bltu-instruction (offset source-register-1 source-register-2)
-  (let ((value1 (integer-register-contents source-register-1))
-        (value2 (integer-register-contents source-register-2)))
-    (when (< value1 value2)
-      (incf *pc* offset))))
-
-(defun execute-bge-instruction (offset source-register-1 source-register-2)
-  (let ((value1
-          (sign-extend-64 (integer-register-contents source-register-1)))
-        (value2
-          (sign-extend-64 (integer-register-contents source-register-2))))
-    (when (>= value1 value2)
-      (incf *pc* offset))))
-
-(defun execute-bgeu-instruction (offset source-register-1 source-register-2)
-  (let ((value1 (integer-register-contents source-register-1))
-        (value2 (integer-register-contents source-register-2)))
-    (when (>= value1 value2)
-      (incf *pc* offset))))
-
-(defun execute-branch-instruction
-    (offset func-3 source-register-1 source-register-2)
-  (ecase func-3
-    (#b000
-     (execute-beq-instruction offset source-register-1 source-register-2))
-    (#b001
-     (execute-bne-instruction offset source-register-1 source-register-2))
-    (#b100
-     (execute-blt-instruction offset source-register-1 source-register-2))
-    (#b101
-     (execute-bge-instruction offset source-register-1 source-register-2))
-    (#b110
-     (execute-bltu-instruction offset source-register-1 source-register-2))
-    (#b111
-     (execute-bgeu-instruction offset source-register-1 source-register-2))))
+(defmacro with-target-and-source-register-values (instruction &body body)
+  `(let* ((target (ins:target instruction))
+          (source-register-1 (ins:source-register-1 ,instruction))
+         (value-1 (integer-register-contents source-register-1))
+          (source-register-2 (ins:source-register-2 ,instruction))
+          (value-2 (integer-register-contents source-register-2)))
+     ,@body))

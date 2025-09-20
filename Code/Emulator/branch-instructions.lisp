c29153989c90e571197f2ae0a7b7934e4c20a6f6
Robert Strandh <robert.strandh@gmail.com>
2025-09-20 16:31:20 +0200

    Define method on execute-instruction for BLTU-INSTRUCTION.

--- Code/Emulator/branch-instructions.lisp
+++ Code/Emulator/branch-instructions.lisp
@@ -33,3 +33,8 @@
   (with-target-and-sign-extended-register-values instruction
     (when (< value-1 value-2)
       (setf *pc* target))))
+
+(defmethod excute-instruction ((instruction ins:bltu-instruction))
+  (with-target-and-register-values instruction
+    (when (< value-1 value-2)
+      (setf *pc* target))))

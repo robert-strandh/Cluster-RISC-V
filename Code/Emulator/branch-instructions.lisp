ad0aa8e4759f900424fdfe1e3acf29af48578721
Robert Strandh <robert.strandh@gmail.com>
2025-09-20 16:25:11 +0200

    Shorten name of macro.

--- Code/Emulator/branch-instructions.lisp
+++ Code/Emulator/branch-instructions.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:cluster-risc-v-emulator)
 
-(defmacro with-target-and-source-register-values (instruction &body body)
+(defmacro with-target-and-register-values (instruction &body body)
   `(let* ((target (ins:target instruction))
           (source-register-1 (ins:source-register-1 ,instruction))
          (value-1 (integer-register-contents source-register-1))
@@ -9,11 +9,11 @@
      ,@body))
 
 (defmethod excute-instruction ((instruction ins:beq-instruction))
-  (with-target-and-source-register-values instruction
+  (with-target-and-register-values instruction
     (when (= value-1 value-2)
       (setf *pc* target))))
 
 (defmethod excute-instruction ((instruction ins:bne-instruction))
-  (with-target-and-source-register-values instruction
+  (with-target-and-register-values instruction
     (unless (= value-1 value-2)
       (setf *pc* target))))

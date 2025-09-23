4eae338701ffd442d0273dc0455c71e4b56c5c87
Robert Strandh <robert.strandh@gmail.com>
2025-09-23 14:50:43 +0200

    Define functions for returning and setting an instruction address.

--- Code/Emulator/addresses.lisp
+++ Code/Emulator/addresses.lisp
@@ -3,3 +3,9 @@
 ;;; During emulation, this variable contains a hash table that maps
 ;;; instructions to addresses.
 (defvar *addresses*)
+
+(defun instruction-address (instruction)
+  (gethash instruction *addresses*))
+
+(defun (setf instruction-address) (address instruction)
+  (setf (gethash instruction *addresses*) address))
